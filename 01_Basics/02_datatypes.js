153e8e75a5a3ab404d726f11d22ca50a4c7819a4
Rajneesh Kumar <157763438+rajneeshkumar09@users.noreply.github.com>
2025-08-16 19:28:58 +0000

    DataTypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treate all JS code as newer version
+
+// alert(3+3)      //we are using node js, not browser
+
+console.log(3
+    +3);    //code readability should be high
+console.log("Abhishek");
+
+/* 
+    standard definitions:=> https://tc39.es/ecma262/ 
+*/
+
+let name = "abhishek"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to the power 53
+//bigint
+//string => "", ''
+//boolean => true, false
+//null => standalone value
+//undefined => not assign value
+//symbol => uniqueness
+
+//Object
+
+console.log(typeof "abhishek")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
