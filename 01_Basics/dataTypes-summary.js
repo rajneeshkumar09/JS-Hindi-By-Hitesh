76ecad65439a9def9d3b746edbe548918bef620c
Rajneesh Kumar <157763438+rajneeshkumar09@users.noreply.github.com>
2025-08-29 07:50:13 +0000

    summary

---
+++ 01_Basics/dataTypes-summary.js
@@ -0,0 +1,7 @@
+// Primitive Data Types => call by value hote hai
+// 7 Types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference Data Types (Non-primitive Data Types)
+// Arrays, Objects, Functions
+
+// JavaScript is a dynamic programming language
