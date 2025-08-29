62856d3a3526a409938024c9d378c23af6422b27
Rajneesh Kumar <157763438+rajneeshkumar09@users.noreply.github.com>
2025-08-29 06:32:58 +0000

    Str-to-num-conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -34,3 +34,45 @@ let someNumber = 25;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// *************** Operations *******************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = 'Hello';
+let str2 = 'Hitesh';
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + '2');
+
+// console.log(+true);
+// console.log(+'');
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/#sec-type-conversion
+
+// MDN increment operation (postfix and prefix); 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
