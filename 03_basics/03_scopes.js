03b4b2787b101cd1d4edbb439ef79fb4cbf0cbf2
RAJNEESH KUMAR <02sameergangwar2003@gmail.com>
2025-09-05 00:31:55 +0530

    scopes basics done

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -1,7 +1,23 @@
-let a = 10;
-const b = 20;
-var c = 30;
+// var c = 300;
 
-console.log(a);
-console.log(b);
-console.log(c);
+let a = 300;
+const b = 20;
+
+if(true) {
+    let a = 10;
+    const b = 20;
+    // var c = 30;
+    console.log("INNER(BLOCK SCOPE VALUE):", a);
+}
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+console.log("OUTER(GLOBAL SCOPE VALUE):",a);
+// console.log(b);
+// console.log(c);
+
+
+/* Browser ke andar console me scope check krte hai bo alg hai. Aur code enviornment me node ke through example ko run krte hai tb scope alg hai i.e. node me scope alg trh se kam krta hai.  */
