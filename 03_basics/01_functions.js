95fe6f7eec40b3b4fb382a9fbde525538138cb60
Rajneesh Kumar <157763438+rajneeshkumar09@users.noreply.github.com>
2025-09-04 14:52:33 +0000

    function basics done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,13 +27,31 @@ function sayMyName() {
 sayMyName();       //known as execution
 
 function addTwoNumbers(number1, number2) {      // (number1, number2) --> known as paramenters (in function declaration)
-    console.log(number1 + number2)
+    // console.log(number1 + number2)
+
+    // let result = number1 + number2;
+    // return result;
+    // console.log("Hitesh");
+
+    return number1 + number2
 }
 
 // addTwoNumbers(3, "4");      // (n, m) --> known as arguments (in function call)
 // addTwoNumbers(3, "a");
 
-addTwoNumbers(3, 5);
+// addTwoNumbers(3, 5);
 const result = addTwoNumbers(3, 5);
 
-console.log("Result :", result);
+// console.log("Result :", result);
+
+// function loginUserMessage(username = "sam") {
+function loginUserMessage(username) {
+    if(!username) {        //if(!username) - has the same meaning as (username === undefined) 
+        console.log("Please enter a username");
+        return 
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Hitesh"));
+console.log(loginUserMessage());
