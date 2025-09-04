9ed6f3a32b56843b15f9ed8b559c8401326ddb78
RAJNEESH KUMAR <02sameergangwar2003@gmail.com>
2025-09-05 01:27:33 +0530

    scope done

---
+++ 03_basics/04_scope.js
@@ -0,0 +1,39 @@
+/* Nested Scope */
+
+function one() {
+    const username = "hitesh";
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+
+// one();
+
+if (true) {
+    const username = "hitesh";
+    if (username === "hitesh") {
+        const website = " youtube";
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+//++++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num) {
+    return num + 1;
+}
+
+/* Expretion */
+addTwo(5);      //can't access before initialization
+
+const addTwo = function(num) {
+    return num + 2;
+}
